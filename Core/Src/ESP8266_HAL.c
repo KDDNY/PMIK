2b9fe4edc3134f177b464e5c734e5256c806aee7
KDDNY <mpiotrowski1488@gmail.com>
2020-12-05 15:39:23 +0100

    s

---
+++ Core/Src/ESP8266_HAL.c
@@ -0,0 +1,163 @@
+/*
+ * ESP8266_HAL.c
+ *
+ *  Created on: Apr 14, 2020
+ *      Author: Controllerstech
+ */
+
+
+#include "UartRingbuffer_multi.h"
+#include "ESP8266_HAL.h"
+#include "stdio.h"
+#include "string.h"
+
+extern UART_HandleTypeDef huart1;
+extern UART_HandleTypeDef huart2;
+
+#define wifi_uart &huart1
+#define pc_uart &huart2
+
+
+char buffer[20];
+float temp;
+
+char *Basic_inclusion = "<!DOCTYPE html> 							\
+						<html> 										\
+						<head> 										\
+						<title>PMIK projekt</title> 				\
+						</head> 									\
+						<body> 										\
+						<h1>PMIK Projekt</h1> 						\
+						<p>Temperatura odczytana z czujnika: </p> 	\
+						</body> 									\
+						</html>";
+
+char *LED_ON = "<p>LED Status: ON</p><a class=\"button button-off\" href=\"/ledoff\">OFF</a>";
+char *LED_OFF = "<p>LED1 Status: OFF</p><a class=\"button button-on\" href=\"/ledon\">ON</a>";
+char *Terminate = "</body></html>";
+
+/*****************************************************************************************************************************************/
+
+void ESP_Init (char *SSID, char *PASSWD)
+{
+	char data[80];
+
+	Ringbuf_init();
+
+	Uart_sendstring("AT+RST\r\n", wifi_uart);
+	Uart_sendstring("RESETTING.", pc_uart);
+	for (int i=0; i<5; i++)
+	{
+		Uart_sendstring(".", pc_uart);
+		HAL_Delay(1000);
+	}
+
+	/********* AT **********/
+	Uart_flush(wifi_uart);
+	Uart_sendstring("AT\r\n", wifi_uart);
+	while(!(Wait_for("OK\r\n", wifi_uart)));
+	Uart_sendstring("AT---->OK\n\n", pc_uart);
+
+
+	/********* AT+CWMODE=1 **********/
+	Uart_flush(wifi_uart);
+	Uart_sendstring("AT+CWMODE=1\r\n", wifi_uart);
+	while (!(Wait_for("OK\r\n", wifi_uart)));
+	Uart_sendstring("CW MODE---->1\n\n", pc_uart);
+
+
+	/********* AT+CWJAP="SSID","PASSWD" **********/
+	Uart_flush(wifi_uart);
+	Uart_sendstring("connecting... to the provided AP\n", pc_uart);
+	sprintf (data, "AT+CWJAP=\"%s\",\"%s\"\r\n", SSID, PASSWD);
+	Uart_sendstring(data, wifi_uart);
+	while (!(Wait_for("OK\r\n", wifi_uart)));
+	sprintf (data, "Connected to,\"%s\"\n\n", SSID);
+	Uart_sendstring(data,pc_uart);
+
+
+	/********* AT+CIFSR **********/
+	Uart_flush(wifi_uart);
+	Uart_sendstring("AT+CIFSR\r\n", wifi_uart);
+	while (!(Wait_for("CIFSR:STAIP,\"", wifi_uart)));
+	while (!(Copy_upto("\"",buffer, wifi_uart)));
+	while (!(Wait_for("OK\r\n", wifi_uart)));
+	int len = strlen (buffer);
+	buffer[len-1] = '\0';
+	sprintf (data, "IP ADDR: %s\n\n", buffer);
+	Uart_sendstring(data, pc_uart);
+
+	/********* AT+CIPMUX **********/
+	Uart_flush(wifi_uart);
+	Uart_sendstring("AT+CIPMUX=1\r\n", wifi_uart);
+	while (!(Wait_for("OK\r\n", wifi_uart)));
+	Uart_sendstring("CIPMUX---->OK\n\n", pc_uart);
+
+	/********* AT+CIPSERVER **********/
+	Uart_flush(wifi_uart);
+	Uart_sendstring("AT+CIPSERVER=1,80\r\n", wifi_uart);
+	while (!(Wait_for("OK\r\n", wifi_uart)));
+	Uart_sendstring("CIPSERVER---->OK\n\n", pc_uart);
+
+	Uart_sendstring("Now Connect to the IP ADRESS\n\n", pc_uart);
+
+}
+
+
+
+
+int Server_Send (char *str, int Link_ID)
+{
+	int len = strlen (str);
+	char data[80];
+	sprintf (data, "AT+CIPSEND=%d,%d\r\n", Link_ID, len);
+	Uart_sendstring(data, wifi_uart);
+	while (!(Wait_for(">", wifi_uart)));
+	Uart_sendstring (str, wifi_uart);
+	while (!(Wait_for("SEND OK", wifi_uart)));
+	sprintf (data, "AT+CIPCLOSE=5\r\n");
+	Uart_sendstring(data, wifi_uart);
+	while (!(Wait_for("OK\r\n", wifi_uart)));
+	return 1;
+}
+
+void Server_Handle (char *str, int Link_ID)
+{
+	char datatosend[1024] = {0};
+		sprintf (datatosend, Basic_inclusion);
+		strcat(datatosend, "<p>");
+		char buffer[100];
+		snprintf(buffer, 10, "%.1f", temp);
+		strcat(datatosend, buffer);
+		strcat(datatosend, "</p>");
+		strcat(datatosend, Terminate);
+		Server_Send(datatosend, Link_ID);
+}
+
+void Server_Start ()
+{
+	char buftocopyinto[64] = {0};
+	char Link_ID;
+	while (!(Get_after("+IPD,", 1, &Link_ID, wifi_uart)));
+	Link_ID -= 48;
+	while (!(Copy_upto(" HTTP/1.1", buftocopyinto, wifi_uart)));
+	if (Look_for("/ledon", buftocopyinto) == 1)
+	{
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 1);
+		Server_Handle("/ledon",Link_ID);
+	}
+
+	else if (Look_for("/ledoff", buftocopyinto) == 1)
+	{
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 0);
+		Server_Handle("/ledoff",Link_ID);
+	}
+
+	else if (Look_for("/favicon.ico", buftocopyinto) == 1);
+
+	else
+	{
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 0);
+		Server_Handle("/ ", Link_ID);
+	}
+}
